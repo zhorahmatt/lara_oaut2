c495e0d5eb1f3c5f42fad118229b773f328e7c4b
zhorahmatt <info.rahmathidayat@gmail.com>
2018-06-11 13:34:14 +0800

    add auth grant that will be activate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+use Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes();
+
+        //token expires in
+        Passport::tokensExpireIn(now()->addDays(15));
+
+        //refresh token expire in
+        Passport::refreshTokensExpireIn(now()->addDays(30));
     }
 }
